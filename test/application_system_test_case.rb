4da4f5b1c5ad7051f8419ae8927e830c0da9b80c
Johan <johand@openmailbox.org>
2023-01-16 21:08:58 -0400

    Running tests with docker

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,14 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  if ENV['SELENIUM_HUB_URL']
+    Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
+
+    options = {
+      browser: :remote,
+      url: ENV['SELENIUM_HUB_URL']
+    }
+  end
+
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options:
 end
