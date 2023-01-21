31820f9b130da9c5f86cd0a2c06285a3079a6832
Johan <johand@openmailbox.org>
2023-01-20 22:27:11 -0400

    Broadcasting created quotes with turbo streams

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,4 +4,10 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(created_at: :desc) }
+  after_create_commit lambda {
+                        broadcast_prepend_to 'quotes',
+                                             partial: 'quotes/quote',
+                                             locals: { quote: self },
+                                             target: 'quotes'
+                      }
 end
