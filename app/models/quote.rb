bbf484009482ea3ea4742de26690e0ecc794fbb6
Johan <johand@openmailbox.org>
2023-01-21 18:58:00 -0400

    Broadcasting quote updates with turbo streams

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,4 +5,5 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(created_at: :desc) }
   after_create_commit -> { broadcast_prepend_to 'quotes' }
+  after_update_commit -> { broadcast_replace_to 'quotes' }
 end
