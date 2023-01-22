b04c314d8a68227ce2ee2ab712fa82a440c07946
Johan <johand@openmailbox.org>
2023-01-22 18:53:53 -0400

    Users, companies and, quotes associations

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,7 @@
 
 class Company < ApplicationRecord
   validates :name, presence: true
+
+  has_many :users, dependent: :destroy
+  has_many :quotes, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_22_010919) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_22_223538) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_22_010919) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.uuid "company_id", null: false
+    t.index ["company_id"], name: "index_quotes_on_company_id"
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -34,8 +36,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_22_010919) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.uuid "company_id", null: false
+    t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "quotes", "companies"
+  add_foreign_key "users", "companies"
 end
